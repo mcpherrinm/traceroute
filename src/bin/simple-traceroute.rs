2c7812dcdee8771ede5d55af6754300430ae332b
Matt McPherrin <git@mcpherrin.ca>
2014-06-28 23:11:52 -0700

    Basically rejigger everything

--- src/bin/simple-traceroute.rs
+++ src/bin/simple-traceroute.rs
@@ -1,69 +1,16 @@
-extern crate libc;
-extern crate native;
-extern crate packet;
-use std::io::net::udp::UdpSocket;
+extern crate traceroute;
 use std::io::net::ip::{IpAddr, Ipv4Addr, SocketAddr};
 
-struct Hop {
-  ip: IpAddr,
-  time: uint,
-}
-
-#[allow(experimental)]
-// Need experimental to set_tll, which is key for this to work.
-fn send_trace(ip: IpAddr, seed: u64) {
-  let addr = SocketAddr { ip: Ipv4Addr(0, 0, 0, 0), port: seed as u16};
-  let mut dest = SocketAddr { ip: ip, port: 12345 };
-  let mut socket = UdpSocket::bind(addr).unwrap();
-
-  let mut buf = [1,2,3,4,5,6,7,8];
-  for ttl in range(1, 30) {
-    socket.set_ttl(ttl).unwrap();
-    dest.port = ttl as u16;
-    buf[0] = ttl as u8;
-    socket.sendto(buf, dest).unwrap();
-    // Probably want to put the time in the buffer here.
-  }
-}
-
 fn main() {
-  let socket = packet::rawsocket::RawSocket::icmp_sock().unwrap();
+  let req=  traceroute::TraceRequest{
+    tries: 3,
+    hop_limit: 30,
+    destination: Ipv4Addr(8,8,8,8)
+  };
+  
+  let res = req.run();
 
-  spawn( proc() {send_trace(Ipv4Addr(8,8,8,8), 12345)} );
+  println!("{}", res);
 
-  let mut buffer = [0, ..4096];
-  let mut responses: Vec<Option<Hop>> = Vec::new();
-  for _ in range(0, 30) {
-    responses.push(None);
-  }
-  loop {
-    let buf = socket.recvfrom(buffer.as_mut_slice());
-    let ip = packet::Ip::new(buf);
-    println!("Snarfed IP:");
-    ip.print();
-    if (ip.version(), ip.protocol()) != (4, 1) { continue };
 
-    let icmp = packet::Icmp::new(ip.payload());
-    icmp.print();
-
-    match icmp.icmp_type() {
-      11 => { // TTL exceeded
-        let innerip = packet::Ip::new(icmp.payload());
-        innerip.print();
-        if innerip.protocol() != 17 { continue };
-        let udp = packet::Udp::new(innerip.payload());
-        udp.print();
-        let ttl = udp.dstport();
-        *responses.get_mut(ttl as uint) = Some( Hop { ip: ip.source(), time: 0 } );
-        for i in responses.iter() {
-          match *i {
-            Some(hop) => println!("{} {} {} {}", hop.ip, hop.time, '*', '*'),
-            None => (), //println!("-"),
-          }
-        }
-        println!("-");
-      },
-      _ => continue,
-    }
-  }
 }
--- src/traceroute.rs
+++ src/traceroute.rs
@@ -2,17 +2,18 @@ extern crate packet;
 use std::io::net::ip::{IpAddr, Ipv4Addr, SocketAddr};
 use std::io::net::udp::UdpSocket;
 
+#[deriving(Show)]
 pub struct Hop {
-  time: uint,
-  where: IpAddr,
+  pub where: IpAddr,
+  pub time: uint,
 }
 
 pub struct TraceRequest {
   /// The number of packets we want to send to each host along the way.
-  tries: uint,
+  pub tries: uint,
   /// How far away we're going to look
-  hop_limit: uint,
-  destination: IpAddr,
+  pub hop_limit: uint,
+  pub destination: IpAddr,
   // Todo: progress callback.  For now, hardcode printing?
 }
 
@@ -22,47 +23,98 @@ impl TraceRequest {
   /// Runs the traceroute, sending in a new task and receiving in
   /// this one.
   pub fn run(&self) -> Vec<Vec<Hop>> {
-    // Start listeclone().ning on a socket first:
+    let mut results = Vec::new();
+    for _ in range(0, self.hop_limit) {
+      results.push(Vec::new());
+    }
+    // Start listening on a socket first:
     let listener = packet::rawsocket::RawSocket::icmp_sock().unwrap();
+    listener.timeout(2);
 
     let sender = *self.clone();
     std::task::spawn(proc() { sender.send_all_probes() });
 
     let mut buf = [8, ..9200];
+    let mut recv_count = 0;
+    let mut failcount = 0;
     loop {
       let pkt = listener.recvfrom(buf.as_mut_slice());
+      if pkt.is_none() {
+        // assuming failures are timeouts...
+        failcount += 1;
+        if failcount > 5 { break }
+        continue
+      }
+      match TraceResponsePkt::new(pkt.unwrap()) {
+        None => continue, // Not a packet we are about
+        Some(resp) => {
+          recv_count += 1;
+          println!("{}: {} @ {}", resp.from(), resp.dist(), resp.time());
+          results.get_mut(resp.dist()).push(Hop{time: resp.time(), where: resp.from()});
+          if recv_count >= self.hop_limit * self.tries {
+            break;
+          }
+        }
+      }
     }
 
-
-   Vec::new()
+    results
   }
 
   // Syncronously send all the probes
   #[allow(experimental)]
   fn send_all_probes(&self) {
     let addr = SocketAddr { ip: Ipv4Addr(0, 0, 0, 0), port: 12345};
-    let mut dest = SocketAddr { ip: self.destination, port: 12345};
+    let mut dest = SocketAddr { ip: self.destination, port: 0};
     let mut socket = UdpSocket::bind(addr).unwrap();
 
     let mut buf = [1,2,3,4,5,6,7,8];
-    for ttl in range(1, self.hop_limit) {
-      for _ in range(0, self.tries) {
+    for _ in range(0, self.tries) {
+      for ttl in range(1, self.hop_limit) {
         socket.set_ttl(ttl as int).unwrap();
-        dest.port = ttl as u16;
+        dest.port = (ttl as u16) + 50000;
         buf[0] = ttl as u8;
         socket.sendto(buf, dest).unwrap();
-        // Probably want to put the time in the buffer here.
+        // Probably want to record the time somehow.
       }
     }
   }
 }
 
 pub struct TraceResponsePkt<'a> {
-  buf: &'a [u8]
+  ip: packet::parser::Ip<'a>,
+  icmp: packet::parser::Icmp<'a>,
+  inner_ip: packet::parser::Ip<'a>,
+  udp: packet::parser::Udp<'a>,
 }
 
 impl<'b> TraceResponsePkt<'b> {
   fn new<'a>(buf: &'a [u8]) -> Option<TraceResponsePkt<'a>> {
-    Some(TraceResponsePkt{buf: buf})
+    let ip = packet::parser::Ip::new(buf);
+    if (ip.version(), ip.protocol()) != (4, 1) { return None; }
+
+    let icmp = packet::parser::Icmp::new(ip.payload());
+    if icmp.icmp_type() != 11 || icmp.icmp_type() != 3 { return None; }
+
+    let inner_ip = packet::parser::Ip::new(icmp.payload());
+    if (ip.version(), ip.protocol()) != (4, 17) { return None; }
+
+    let udp = packet::parser::Udp::new(inner_ip.payload());
+    // if ... this UDP packet is one we sent
+    // surely involving whatever scheme for recording time we have.
+
+    Some(TraceResponsePkt{ip: ip, icmp: icmp, inner_ip: inner_ip, udp: udp})
+  }
+
+  pub fn from(&self) -> IpAddr {
+    self.ip.source()
+  }
+
+  pub fn dist(&self) -> uint {
+    self.inner_ip.ttl() as uint
+  }
+
+  pub fn time(&self) -> uint {
+    0
   }
 }
